984020a859cb61877745d990a7bb6a2140e02336
karthik-karotra <karthikpatel54@gmail.com>
2020-01-30 15:18:29 +0530

    ADDED:Function for getting value after rolling the die

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -4,3 +4,9 @@ echo "Welcome to Snake and Ladder Simulator"
 #CONSTANTS
 NO_OF_PLAYERS=1
 START_POSITION=0
+
+function rollingTheDie() {
+	dieValue=$((RANDOM%6+1))
+	echo "Die number generated is $dieValue"
+}
+rollingTheDie
