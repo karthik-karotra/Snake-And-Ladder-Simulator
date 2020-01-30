c6956b6e5a89f933e482de7efb5911f09a5495a4
karthik-karotra <karthikpatel54@gmail.com>
2020-01-30 15:43:12 +0530

    ADDED:Function for checking whether its a no play,snake or ladder

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -4,9 +4,30 @@ echo "Welcome to Snake and Ladder Simulator"
 #CONSTANTS
 NO_OF_PLAYERS=1
 START_POSITION=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+
+currentPosition=0
 
 function rollingTheDie() {
 	dieValue=$((RANDOM%6+1))
 	echo "Die number generated is $dieValue"
+	checkingNoPlayOrLadderOrSnake
+}
+
+function checkingNoPlayOrLadderOrSnake() {
+	case $((RANDOM%3)) in
+		$NO_PLAY) 
+			currentPosition=$currentPosition
+			;;
+		$SNAKE)
+			currentPosition=$(($currentPosition - $dieValue))
+			;;
+		$LADDER)
+			currentPosition=$(($currentPosition + $dieValue))
+			;;
+	esac
 }
 rollingTheDie
+
