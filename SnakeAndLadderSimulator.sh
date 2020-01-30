0db3a9fb10acf0ce9daf59ee6b39cc348cc8a345
karthik-karotra <karthikpatel54@gmail.com>
2020-01-30 17:22:19 +0530

    ADDED:Repeating condition till player reaches winning position 100

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Snake and Ladder Simulator"
 
 #CONSTANTS
@@ -7,6 +7,7 @@ START_POSITION=0
 NO_PLAY=0
 SNAKE=1
 LADDER=2
+WINNING_POSITION=100
 
 currentPosition=0
 
@@ -18,16 +19,33 @@ function rollingTheDie() {
 
 function checkingNoPlayOrLadderOrSnake() {
 	case $((RANDOM%3)) in
-		$NO_PLAY) 
+		$NO_PLAY)
+			echo "There's no progress in your position" 
 			currentPosition=$currentPosition
+			echo $currentPosition
 			;;
 		$SNAKE)
+			echo "OOPS!!!You were eaten by a snake"
 			currentPosition=$(($currentPosition - $dieValue))
+			restartsToZero
+			echo $currentPosition
 			;;
 		$LADDER)
+			echo "Yeah!! You just climb a ladder"
 			currentPosition=$(($currentPosition + $dieValue))
+			echo $currentPosition
 			;;
 	esac
 }
-rollingTheDie
 
+function restartsToZero() {
+	if [ $currentPosition -lt 0 ]
+	then
+		currentPosition=$(($currentPosition + $dieValue))
+	fi
+}
+
+while [ $currentPosition -ne $WINNING_POSITION ]
+do
+	rollingTheDie
+done
