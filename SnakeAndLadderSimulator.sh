bfabcd22f92af9a3ac89c7a64a4028bf1a32e7a8
karthik-karotra <karthikpatel54@gmail.com>
2020-02-07 10:35:48 +0530

    REFACTORED:Removed un-necessary reused code

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -9,7 +9,7 @@ SNAKE=1
 LADDER=2
 WINNING_POSITION=100
 
-#VARIABLE
+#VARIABLES
 dieCount=0
 
 function rollingTheDie() {
@@ -21,19 +21,17 @@ function checkingNoPlayOrLadderOrSnake() {
 	case $((RANDOM%3)) in
 		$NO_PLAY)
 			player=$1
-			echo $player
 			;;
 		$SNAKE)
 			player=$(($1 - $2))
 			restartsToZeroOrTillExactWinningPosition -lt 0 +
-			echo $player
 			;;
 		$LADDER)
 			player=$(($1 + $2))
 			restartsToZeroOrTillExactWinningPosition -gt 100 -
-			echo $player
 			;;
 	esac
+	echo $player
 }
 
 function restartsToZeroOrTillExactWinningPosition() {
@@ -50,16 +48,15 @@ function startGame() {
 	SWITCH=-1
 	while [[ $player1 -ne $WINNING_POSITION && $player2 -ne $WINNING_POSITION ]]
 	do
+		rollingTheDie
 		if [ $currentPlayer -eq -1 ]
 		then
-			rollingTheDie
 			player1=$(checkingNoPlayOrLadderOrSnake $player1 $dieValue)
 			if [ $player1 -eq $WINNING_POSITION ]
 			then
 				echo "Player 1 Wins"
 			fi
 		else
-			rollingTheDie
 			player2=$(checkingNoPlayOrLadderOrSnake $player2 $dieValue)
 			if [ $player2 -eq $WINNING_POSITION ]
 			then
